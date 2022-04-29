5171f6fa480222bf0d038ab0b423e8a361c0d9dc
muhammedikbalaktas <93763631+muhammedikbalaktas@users.noreply.github.com>
2022-04-29 12:32:31 +0300

    Add files via upload

---
+++ PrimeNumber/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static boolean isPrime(int number){
+        if(number<=1)return false;
+
+        for(int i=2;i<number;i++){
+            if(number%i==0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        for(int i=0;i<20;i++){
+            System.out.println("i: "+i+" "+isPrime(i));
+        }
+    }
+}
